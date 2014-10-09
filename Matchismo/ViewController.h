427317cef1a69edecbd50acea134372018e326d5
Alex K <khoralcode@gmail.com>
2014-10-08 18:25:45 -0700

    Initial Commit

---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Alex on 10/8/14.
+//  Copyright (c) 2014 Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Matchismo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Alex on 10/8/14.
+//  Copyright (c) 2014 Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
